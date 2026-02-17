07d92466247d140f6fa17596bec3ad781485ba18
kritvi paliwal <kritvipaliwal2005@gmail.com>
2026-02-17 11:12:56 +0530

    add render port support

--- to do app/backend/server.js
+++ to do app/backend/server.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 const TASKS_FILE = path.join(__dirname, 'tasks.json');
 
 // Middleware
